0b7e1a0e741b8b8c89f147bd055d59298b50d94e
Simón Villafañe <sivinoreply@gmail.com>
2022-02-16 01:18:28 -0300

    feat(Root): app init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./public/index.html', './src/**/*.svelte'],
+  theme: {
+    extend: {
+      boxShadow: {
+        'ctm': '8px 8px 25px -5px rgba(8, 145, 178, 0.50)'
+      }
+    },
+  },
+  plugins: [],
+}
