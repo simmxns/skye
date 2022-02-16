5d006842299ccb787bf38d80d2c0e6d5f44903d5
Simón Villafañe <sivinoreply@gmail.com>
2022-02-16 01:19:55 -0300

    feat(Root): awesome greet

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	const name = "Simon";
+</script>
+
+<div class="flex items-center justify-center rounded-xl bg-cyan-900 p-5 shadow-ctm">
+	<h1 class="text-3xl font-semibold text-cyan-600">
+		Hi {name}!
+	</h1>
+</div>
+
+<style lang="postcss" global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
